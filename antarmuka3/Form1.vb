f474d18b551c4c4fbf7727091779b7097e25928c
azzar budiyanto <48999412+1999AZZAR@users.noreply.github.com>
2020-09-17 11:56:16 +0700

    Add files via upload

---
+++ antarmuka3/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Main
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub L01INPUTOUTPUTToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles L01INPUTOUTPUTToolStripMenuItem.Click
+        L01_18518241046.Show()
+    End Sub
+End Class
